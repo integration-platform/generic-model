72c6587c2a35fab9f6e1aa1a3a42f62813218645
Stavros Anastasiadis <sanastasiadis@gmail.com>
2016-07-15 01:18:53 +0200

    initial commit

---
+++ src/main/java/org/stavros/interplat/model/Model.java
@@ -0,0 +1,7 @@
+package org.stavros.interplat.model;
+
+import java.util.ArrayList;
+
+public class Model extends ArrayList<Record> {
+
+}
---
+++ src/main/java/org/stavros/interplat/model/Record.java
@@ -0,0 +1,7 @@
+package org.stavros.interplat.model;
+
+import java.util.HashMap;
+
+public class Record extends HashMap<String,Object> {
+
+}
